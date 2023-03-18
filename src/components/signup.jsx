e0df80b62baa6f33e31a8fbe697f446b14a3c392
Muneeb Aslam <muneeb.official.686332@hotmail.com>
2023-03-18 12:19:00 +0500

    Sign Up Done

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 export default function SignUp(){
     return (
-        <div className="signup">
-            Sign Up
-        </div>
+            <form action="" className="signup">
+                <h3>Create An Account</h3>
+                <input type="text" placeholder="Name"/>
+                <input type="email" placeholder="Email Address" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Create Account</button>
+                <div className="signin">
+                    Already Have an Account? <a href="">Sign In</a>
+                </div>
+            </form>
     )
 }
