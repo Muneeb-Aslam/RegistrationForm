27f76ec7f3605a4c648c82918c7f92fbb7cdd889
Muneeb Aslam <muneeb.official.686332@hotmail.com>
2023-03-18 11:53:31 +0500

    initial Files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUp from './components/signup'
+import SignIn from './components/signin'
+import './App.css'
+
+function App() {
+  return (  
+    <SignUp />
+  )
+}
+
+export default App
